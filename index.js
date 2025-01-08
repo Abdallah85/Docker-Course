e268066938b7584ad67a384252dae8b5c472e5d0
Abdallah Mohamed <abdallah.moh.153@gmail.com>
2025-01-08 17:36:46 +0200

    update commands.txt with hot reload instructions; modify index.js response and server port; adjust start script for nodemon

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello Beso From Egypt and this is my first Node.js App ohhh");
 });
 
 app.listen(4000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 4000");
 });
